99b6d6775a01a56b71c7a6079ac4982346a20ba0
Danielle van Graan <Danielle.vanGraan@dstvdm.com>
2018-02-10 17:26:22 +0200

    Init

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,7 @@
+namespace Vega.Controllers
+{
+    public class MakesController
+    {
+        
+    }
+}
---
+++ Persistance/VegaDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Vega.Models;
+
+namespace Vega.Persistance
+{
+    public class VegaDbContext :DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options)
+                            :base(options)
+        {
+
+        }
+        public DbSet<Make> Makes { get; set; }
+    }
+}
